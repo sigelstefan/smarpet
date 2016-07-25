5003f3bca18dea5d169bc8782fa9e439952d0722
Sigel Stefan <sigel.stefan@gmail.com>
2016-07-25 18:31:10 +0200

    Updates

--- nodejs/center.js
+++ nodejs/center.js
@@ -16,7 +16,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json())
 
-// First you need to create a connection to the db
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -24,104 +23,175 @@ var con = mysql.createConnection({
     database: "smarpet_center"
 });
 
+
 app.get('/smarpet/center/patients', function (req, res) {
     var query = "SELECT * FROM patient;";
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows));
     });
 });
 
 app.get('/smarpet/center/patient/:id/info', function (req, res) {
     var query = "SELECT * FROM patient WHERE id = " + req.params.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows))
     });
 });
 
 app.get('/smarpet/center/patient/:id/contact', function (req, res) {
     var query = "SELECT * FROM contact WHERE id_patient = " + req.params.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows))
     });
 });
 
-app.get('/smarpet/center/patient/:id/activities', function (req, res){
+app.get('/smarpet/center/patient/:id/activities', function (req, res) {
     var query = "SELECT time, distance FROM activities WHERE time >= NOW() - INTERVAL 24 HOUR AND id_patient = " + req.params.id + " ORDER BY time asc;";
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         var result = [];
-        for(var i in rows){
-            result.push([new Date(rows[i].time).getTime(),rows[i].distance]);
+        for (var i in rows) {
+            result.push([new Date(rows[i].time).getTime(), rows[i].distance]);
         }
         res.send(result)
     });
 });
 
 /**
-* READ, CREATE AND DELETE drugs of a patient
-*/
+ * READ, CREATE AND DELETE drugs of a patient
+ */
 app.get('/smarpet/center/patient/:id/drugs', function (req, res) {
     var query = "SELECT d.id, d.name FROM drug d JOIN patient_has_drug phd on d.id = phd.id_drug WHERE phd.id_patient = " + req.params.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows))
     });
 });
-app.post('/smarpet/center/patient/drug/add',function(req,res){
+app.post('/smarpet/center/patient/drug/add', function (req, res) {
     var name = req.body.name;
     var patient = req.body.id;
     var query = "INSERT INTO drug (name) VALUES ('" + name + "')";
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         var query2 = "INSERT INTO patient_has_drug (id_patient, id_drug) VALUES (" + patient + "," + rows.insertId + ")";
-        con.query(query2, function(err2, rows2, fields2) {
-            res.send({ id:rows.insertId });
+        con.query(query2, function (err2, rows2, fields2) {
+            res.send({
+                id: rows.insertId
+            });
         });
     });
 });
-app.post('/smarpet/center/patient/drug/remove',function(req,res){
+app.post('/smarpet/center/patient/drug/remove', function (req, res) {
     var query = "DELETE FROM drug WHERE id = " + req.body.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send()
     });
 });
 
 
 /**
-* READ, CREATE AND DELETE diseases of a patient
-*/
+ * READ, CREATE AND DELETE diseases of a patient
+ */
 app.get('/smarpet/center/patient/:id/diseases', function (req, res) {
     var query = "SELECT d.id, d.name FROM disease d LEFT JOIN patient_has_disease phd on d.id = phd.id_disease WHERE phd.id_patient = " + req.params.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows))
     });
 });
-app.post('/smarpet/center/patient/disease/add',function(req,res){
+app.post('/smarpet/center/patient/disease/add', function (req, res) {
     var name = req.body.name;
     var patient = req.body.id;
     var query = "INSERT INTO disease (name) VALUES ('" + name + "')";
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         var query2 = "INSERT INTO patient_has_disease (id_patient, id_disease) VALUES (" + patient + "," + rows.insertId + ")";
-        con.query(query2, function(err2, rows2, fields2) {
-            res.send({ id:rows.insertId });
+        con.query(query2, function (err2, rows2, fields2) {
+            res.send({
+                id: rows.insertId
+            });
         });
     });
 });
-app.delete('/smarpet/center/patient/disease/remove',function(req,res){
+app.delete('/smarpet/center/patient/disease/remove', function (req, res) {
     var query = "DELETE FROM disease WHERE id = " + req.body.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send()
     });
 });
 
 app.get('/smarpet/center/emergencies', function (req, res) {
-    var query = "SELECT e.id, e.time, p.firstname, p.lastname FROM emergency e LEFT JOIN patient p on e.id_patient = p.id;";
-    con.query(query, function(err, rows, fields) {
+    var query = "SELECT e.id, e.time, p.firstname, p.lastname FROM emergency e LEFT JOIN patient p on e.id_patient = p.id ORDER BY e.time DESC;";
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows));
     });
 });
 
-app.get('/smarpet/center/emergencies/:id/details', function(req, res){
+app.get('/smarpet/center/emergencies/:id/details', function (req, res) {
     var query = "SELECT e.id, e.time, e.event, p.firstname as pFirstname, p.lastname as pLastname, p.Address as pAddress, p.Phone as pPhone, p.yearOfBirth, c.firstname as cFirstname, c.lastname as cLastname, c.address as cAddress, c.phone as cPhone FROM emergency e  LEFT JOIN patient p on e.id_patient = p.id LEFT JOIN contact c on p.id = c.id_patient WHERE e.id = " + req.params.id;
-    con.query(query, function(err, rows, fields) {
+    con.query(query, function (err, rows, fields) {
         res.send(JSON.stringify(rows));
+    });
+});
+
+app.get('/smarpet/center/checkemergency', function (req, res) {
+    var query = "SELECT e.id as id, e.time as time, e.event as event, p.firstname as firstname, p.lastname as lastname FROM emergency e LEFT JOIN patient p ON e.id_patient=p.id WHERE e.called=0;";
+    con.query(query, function (err, rows, fields) {
+        if (rows.length > 0) {
+            for (var i in rows) {
+                var query2 = "UPDATE emergency SET called=1 WHERE id=" + rows[i].id + ";";
+                con.query(query2);
+            }
+            res.send(rows);
+        } else {
+            res.send();
+        }
+    });
+})
+
+app.post('/smarpet/center/fall', function (req, res) {
+    console.log(req.body.floorid);
+    var today = new Date();
+    today.setMonth(today.getMonth() + 1);
+    var time = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(time);
+    var query = "SELECT id FROM patient WHERE floorid='" + req.body.floorid + "';";
+    //var query = "SELECT p.id as pId, p.firstname as pFirstname, p.lastname as pLastname, c.firstname as cFirstname, c.lastname as cLastname, c.email as cEmail FROM patient p LEFT JOIN contact c ON p.id=c.id_patient WHERE p.floorid='" + req.body.floorid + "';";
+    console.log(query);
+    con.query(query, function (err, rows, fields) {
+        if (rows.length > 0) {
+            var query2 = "INSERT INTO emergency (time, event, id_patient) VALUES ('" + time + "','Sturz'," + rows[0].id + ")";
+            con.query(query2, function (err2, rows2, fields2) {
+                if (err2) {
+                    console.log(err2);
+                } else {
+                    console.log("success");
+                }
+                res.send();
+            });
+        } else {
+            console.log("floorid doesn't exist!!!");
+            res.send();
+        }
+    });
+});
+app.post('/smarpet/center/stagger', function (req, res) {
+    console.log(req.body.floorid);
+    var today = new Date();
+    today.setMonth(today.getMonth() + 1);
+    var time = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDate() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(time);
+    var query = "SELECT id FROM patient WHERE floorid='" + req.body.floorid + "';";
+    con.query(query, function (err, rows, fields) {
+        if (rows.length > 0) {
+            var query2 = "INSERT INTO emergency (time, event, id_patient) VALUES ('" + time + "','Schwindelgefühl'," + rows[0].id + ")";
+            con.query(query2, function (err2, rows2, fields2) {
+                if (err2) {
+                    console.log(err2);
+                } else {
+                    console.log("success");
+                }
+                res.send();
+            });
+        } else {
+            console.log("floorid doesn't exist!!!");
+            res.send();
+        }
     });
 });
 
--- nodejs/floor.js
+++ nodejs/floor.js
@@ -8,6 +8,8 @@ var xml2js = require('xml2js');
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET");
+
     next();
 });
 app.use(bodyParser.urlencoded({
@@ -19,37 +21,76 @@ app.use(bodyParser.json())
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "root",
-    database: "center_db"
+    password: "",
+    database: "smarpet_center"
 });
 
 app.get('/smarpet/floor/settings', function (req, res) {
     var parser = new xml2js.Parser();
     fs.readFile(__dirname + '/settings.xml', function (err, data) {
-        if (err) console.log(err);
-        parser.parseString(data, function (err, result) {
-            if (err) console.log(err);
-            var json = JSON.stringify(result)
-            res.send(json);
-        });
+        if (err){
+            console.log("no file found");
+            res.send();
+        }
+        else{
+            parser.parseString(data, function (err, result) {
+                if (err) console.log(err);
+                var json = JSON.stringify(result)
+                res.send(json);
+            });
+        }
     });
 })
 
-app.post('/smarpet/floor/settings', function (req, res) {
 
-    var builder = require('xmlbuilder');
-    var xml = builder.create('settings').ele(req.body).end({
-        pretty: true
-    });
-    fs.writeFile('settings.xml', xml, function (err) {
-        console.log(xml);
-        if (err)
-            return console.log(err);
+app.post('/smarpet/floor/settings/insert', function (req, res) {
+    console.log("insert")
+
+    //ZUERST IN DB
+    //DANN IN XML
+    var query = "INSERT INTO patient (firstname, lastname, address, phone, yearOfBirth, floorid) VALUES ('" + req.body.person.firstname + "','" + req.body.person.lastname + "','" + req.body.person.address + "','" + req.body.person.phone + "','" + req.body.person.yearOfBirth + "','" + req.body.person.floorid + "');";
+    con.query(query, function (err, rows, fields) {
+        console.log(err);
+        var query2 = "INSERT INTO contact (firstname, lastname, address, phone, email, id_patient) VALUES ('" + req.body.contact.firstname + "','" + req.body.contact.lastname + "','" + req.body.person.address + "','" + req.body.contact.phone + "','" + req.body.contact.email + "'," + rows.insertId + ");";
+        con.query(query2, function (err2, rows2, fields2) {
+            console.log(err2);
+            console.log(rows.insertId);
+            var builder = require('xmlbuilder');
+            req.body.person.id = rows.insertId;
+            var xml = builder.create('settings').ele(req.body).end({
+                pretty: true
+            });
+            fs.writeFile('settings.xml', xml, function (err) {
+                console.log(err);
+                res.send();
+            });
+        });
     });
 
-    res.sendStatus(200);
+
+});
+app.post('/smarpet/floor/settings/update', function (req, res) {
+    console.log("update")
+    console.log(req.body.person.id);
+        
+    var query = "UPDATE patient SET firstname='"+req.body.person.firstname+"',lastname='"+req.body.person.lastname+"',address='"+req.body.person.address+"',phone='"+req.body.person.phone+"',yearOfBirth='"+req.body.person.yearOfBirth+"',floorid='"+req.body.person.floorid+"' WHERE id="+req.body.person.id+";"
+    console.log(query);
+    con.query(query, function(err, rows, fields){
+        console.log(rows);
+        var query2 = "UPDATE contact SET firstname='"+req.body.contact.firstname+"',lastname='"+req.body.contact.lastname+"',address='"+req.body.contact.address+"',phone='"+req.body.contact.phone+"',email='"+req.body.contact.email+"' WHERE id_patient="+req.body.person.id+";"
+        con.query(query2, function(err2, rows2, fields2){
+            console.log(rows2);
+            var builder = require('xmlbuilder');
+            var xml = builder.create('settings').ele(req.body).end({
+                pretty: true
+            });
+            fs.writeFile('settings.xml', xml, function (err) {
+                res.send();
+            });
+        });
+    });
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001!');
 });
